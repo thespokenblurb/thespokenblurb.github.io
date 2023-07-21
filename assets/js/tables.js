79060327909abcf5865d49c0e1e9192febecc896
thespokenblurb <140203834+thespokenblurb@users.noreply.github.com>
2023-07-21 14:20:40 -0500

    rest

---
+++ assets/js/tables.js
@@ -0,0 +1,9 @@
+// https://datatables.net/examples/basic_init/dom.html
+
+new DataTable('#table_episodes', {
+    pagingType: 'full_numbers',
+    pageLength: 5,
+    bLengthChange: false,
+    order: [[1, 'desc']]
+});
+
